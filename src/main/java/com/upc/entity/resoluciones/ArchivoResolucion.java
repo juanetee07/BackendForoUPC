a188eda704d23406d630b62d7b2861f96179993c
Tiag4 <longonjulian@gmail.com>
2026-06-08 10:05:19 -0300

    feat: implementar entidad archivo resolucion

---
+++ src/main/java/com/upc/entity/resoluciones/ArchivoResolucion.java
@@ -0,0 +1,32 @@
+package com.upc.demo.entity.resoluciones;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "archivos_resoluciones")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ArchivoResolucion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_archivo")
+    private Long idArchivo;
+
+    @ManyToOne
+    @JoinColumn(name = "resolucion_id")
+    private ArchivoResolucion resolucion;
+
+    @Column(nullable = false, length = 255)
+    private String nombre;
+
+    @Column(nullable = false, length = 500)
+    private String url;
+
+    @Column(nullable = false, length = 100)
+    private String tipo;
+}
