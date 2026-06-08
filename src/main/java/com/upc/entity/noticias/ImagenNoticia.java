3f4a5f69f76d2291f810d332b5ad1f073c88e851
MayraMoy <mayramoyano2511@gmail.com>
2026-06-08 14:33:53 -0300

    Terminada la implementacion de entidad imagen noticia

---
+++ src/main/java/com/upc/entity/noticias/ImagenNoticia.java
@@ -0,0 +1,29 @@
+package com.upc.entity.noticias;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "imagenes_noticias")
+
+public class ImagenNoticia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String url;
+    private String epigrafe;
+
+    /*Muchas imagenes pueden pertenecer a una noticia*/
+    @ManyToOne
+    @JoinColumn(name="noticia_id")
+    private Noticia noticia;
+
+}
