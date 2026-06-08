c7f4c8effb4771b49daa62bf5c7d02ddfef850c9
MayraMoy <mayramoyano2511@gmail.com>
2026-06-08 14:20:01 -0300

    Terminada la implementacion de entidad archivo noticia

---
+++ src/main/java/com/upc/entity/noticias/ArchivoNoticia.java
@@ -0,0 +1,31 @@
+package com.upc.entity.noticias;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "archivos_noticias")
+public class ArchivoNoticia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nombre;
+    private String url;
+    private String tipo;
+
+    /*Muchos archivos pueden pertenecer a una noticia*/
+    @ManyToOne
+    @JoinColumn(name="noticia_id")
+    private Noticia noticia;
+}
+
+
+
