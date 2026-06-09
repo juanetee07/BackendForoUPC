5023ce0fba2612b268e07e270f3c72981fb649f4
juanetee07 <juancruzlarcher07@gmail.com>
2026-06-09 17:13:43 -0300

    Agregando entidad Auditoria y relacion inversa en Usuario

---
+++ src/main/java/com/upc/demo/entity/usuarios/Auditoria.java
@@ -0,0 +1,39 @@
+package com.upc.demo.entity.usuarios;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "auditoria")
+@Getter
+@Setter
+public class Auditoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAuditoria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    private String entidad;
+
+    private Long idRegistro;
+
+    private String accion;
+
+    private LocalDateTime fechaHora;
+
+    private String detalle;
+
+    private String valorAnterior;
+
+    private String valorNuevo;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Auditoria> auditorias;
+}
