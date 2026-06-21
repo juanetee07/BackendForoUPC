178333137ecab1fe0ddb1ff41ae517b1d52f6f6d
MayraMoy <mayramoyano2511@gmail.com>
2026-06-21 16:23:53 -0300

    el metodo editar noticia de la capa de servicios implementada

--- src/main/java/com/upc/demo/service/noticias/INoticiaServicio.java
+++ src/main/java/com/upc/demo/service/noticias/INoticiaServicio.java
@@ -5,4 +5,6 @@ import com.upc.demo.entity.noticias.Noticia;
 public interface INoticiaServicio {
 
     Noticia crearNoticia(Noticia noticia, Long idAutor);
+
+    Noticia editarNoticia(Long idNoticia, Noticia noticiaActualizada, Long idAutor);
 }
--- src/main/java/com/upc/demo/service/noticias/NoticiaServicio.java
+++ src/main/java/com/upc/demo/service/noticias/NoticiaServicio.java
@@ -54,6 +54,18 @@ public class NoticiaServicio implements INoticiaServicio{
         Usuario autor = usuarioRepo.findById(idAutor)
                 .orElseThrow(() -> new RuntimeException("Autor no encontrado"));
 
+        /*Se debe de verificar si el usuario tiene asignado un rol*/
+        if (autor.getRol() == null) {
+            throw new RuntimeException("El usuario no tiene un rol asignado");
+        }
+
+        String rol = autor.getRol().getNombre();
+
+        /*Se verifica que el usuario tenga los permisos necesarios */
+        if (!rol.equalsIgnoreCase("ADMINISTRADOR") && !rol.equalsIgnoreCase("PERSONAL_ESCUELA")) {
+            throw new RuntimeException("No tiene permisos para crear noticias");
+        }
+
         noticia.setCategoriaNoticia(categoria);
         noticia.setAutor(autor);
 
@@ -61,6 +73,61 @@ public class NoticiaServicio implements INoticiaServicio{
         noticia.setFechaCreacion(LocalDateTime.now());
         noticia.setEstado(EstadoNoticia.BORRADOR);
 
+        /*Se guarda la noticia creada*/
         return noticiaRepo.save(noticia);
+    }
+
+    @Override
+    public Noticia editarNoticia(Long idNoticia, Noticia noticiaActualizada, Long idAutor) {
+
+        /*Se debe de buscar que la noticia exista*/
+        Noticia noticiaExistente = noticiaRepo.findById(idNoticia).orElseThrow(
+                () -> new RuntimeException("Noticia no encontrada"));
+
+        /*Se debe de buscar el usuario que exista*/
+        Usuario usuario = usuarioRepo.findById(idAutor).orElseThrow(
+                () -> new RuntimeException("Usuario no encontrado"));
+
+        /*Si el titulo de la noticia no existe o si el titulo de esa noticia esta vacia entonces no podemos guardar la actualizacion */
+        if (noticiaActualizada.getTitulo() == null || noticiaActualizada.getTitulo().trim().isEmpty()) {
+            throw new RuntimeException("El título de la noticia es obligatorio");
+        }
+
+        if (noticiaActualizada.getCuerpo() == null || noticiaActualizada.getCuerpo().trim().isEmpty()) {
+            throw new RuntimeException("El cuerpo de la noticia es obligatorio");
+        }
+
+        if (noticiaActualizada.getCategoriaNoticia() == null || noticiaActualizada.getCategoriaNoticia().getId() == null) {
+            throw new RuntimeException("Debe seleccionar una categoría válida");
+        }
+
+        CategoriaNoticia categoria = categoriaRepo.findById(noticiaActualizada.getCategoriaNoticia().getId()).orElseThrow(
+                () -> new RuntimeException("Categoría no encontrada"));
+
+        /*Se debe de verificar si el usuario tiene asignado un rol*/
+        if (usuario.getRol() == null) {
+            throw new RuntimeException("El usuario no tiene un rol asignado");
+        }
+
+        String rol = usuario.getRol().getNombre();
+
+        /*Se verifica que el usuario tenga los permisos necesarios */
+        if (!rol.equalsIgnoreCase("ADMINISTRADOR") && !rol.equalsIgnoreCase("PERSONAL_ESCUELA")) {
+            throw new RuntimeException("No tiene permisos para editar noticias");
+        }
+
+        /*Se verifica si el usuario PERSONAL_ESCUELA redacto esa noticia para poder editarla*/
+        if (rol.equalsIgnoreCase("PERSONAL_ESCUELA") && !noticiaExistente.getAutor().getIdUsuario().equals(usuario.getIdUsuario())) {
+            throw new RuntimeException("Solo puede editar sus propias noticias");
+        }
+
+        /*Actualizamos*/
+        noticiaExistente.setTitulo(noticiaActualizada.getTitulo());
+        noticiaExistente.setCuerpo(noticiaActualizada.getCuerpo());
+        noticiaExistente.setCategoriaNoticia(categoria);
+
+        noticiaExistente.setFechaActualizacion(LocalDateTime.now());
+
+        return noticiaRepo.save(noticiaExistente);
     }
 }
