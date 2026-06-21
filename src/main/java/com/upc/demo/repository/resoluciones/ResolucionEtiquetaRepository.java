5b05cb457586ca5d8cbbe08e5c2073c10f5ae7bb
Tiag4 <tiag4longo@gmail.com>
2026-06-20 21:41:03 -0300

    feat(resoluciones): agregar consultas en ResolucionEtiquetaRepository

--- src/main/java/com/upc/demo/repository/resoluciones/ResolucionEtiquetaRepository.java
+++ src/main/java/com/upc/demo/repository/resoluciones/ResolucionEtiquetaRepository.java
@@ -4,6 +4,27 @@ import com.upc.demo.entity.resoluciones.ResolucionEtiqueta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ResolucionEtiquetaRepository extends JpaRepository<ResolucionEtiqueta, Long> {
+
+    /**
+     * Obtiene todas las etiquetas asociadas
+     * a una resolución específica.
+     *
+     * Ejemplo:
+     * Resolución 15
+     */
+    List<ResolucionEtiqueta> findByResolucionIdResolucion(Long idResolucion);
+
+    /**
+     * Obtiene todas las resoluciones asociadas
+     * a una etiqueta específica.
+     *
+     * Ejemplo:
+     * Etiqueta "Becas"
+     */
+    List<ResolucionEtiqueta> findByEtiquetaIdEtiqueta(Long idEtiqueta);
+
 }
