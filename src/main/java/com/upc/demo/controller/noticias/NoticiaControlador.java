f0639635afa529cbf233f12a5441e9ce75ee5ea6
MayraMoy <mayramoyano2511@gmail.com>
2026-06-23 12:59:50 -0300

    get consultar noticia capa de controladores

--- src/main/java/com/upc/demo/controller/noticias/NoticiaControlador.java
+++ src/main/java/com/upc/demo/controller/noticias/NoticiaControlador.java
@@ -5,6 +5,7 @@ import com.upc.demo.service.noticias.INoticiaServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,14 @@ public class NoticiaControlador {
         List<Noticia> noticias = noticiaServicio.listarNoticias();
 
         return ResponseEntity.ok(noticias);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Noticia> consultarNoticia(
+            @PathVariable Long id){
+
+        return ResponseEntity.ok(noticiaServicio.consultarNoticia(id)
+        );
     }
 
 }
