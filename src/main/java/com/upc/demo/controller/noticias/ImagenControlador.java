84b54dc0ce824814a9cfaee4595d391643386083
MayraMoy <mayramoyano2511@gmail.com>
2026-06-23 22:20:29 -0300

    post agregar imagen capa controladores

---
+++ src/main/java/com/upc/demo/controller/noticias/ImagenControlador.java
@@ -0,0 +1,27 @@
+package com.upc.demo.controller.noticias;
+
+import com.upc.demo.entity.noticias.ImagenNoticia;
+import com.upc.demo.service.noticias.IImagenServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/imagen-noticias")
+public class ImagenControlador {
+
+    @Autowired
+    private IImagenServicio imagenServicio;
+
+    @PostMapping
+    public ResponseEntity<ImagenNoticia> modificarImagen(
+            @RequestParam Long idNoticia,
+            @RequestBody ImagenNoticia imagenNoticia) {
+
+        ImagenNoticia imagenActualizada = imagenServicio.modificarImagen(idNoticia, imagenNoticia);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(imagenActualizada);
+    }
+}
