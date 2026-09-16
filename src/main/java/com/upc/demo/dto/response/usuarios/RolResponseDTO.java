0b264f8fa7ed373f2d9af9bec9b3e605f3b2d051
juanetee07 <juancruzlarcher07@gmail.com>
2026-09-16 19:04:55 -0300

    feat: construir DTOs de request y response para rol

---
+++ src/main/java/com/upc/demo/dto/response/usuarios/RolResponseDTO.java
@@ -0,0 +1,16 @@
+package com.upc.demo.dto.response.usuarios;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RolResponseDTO {
+
+    private Long idRol;
+    private String nombre;
+}
