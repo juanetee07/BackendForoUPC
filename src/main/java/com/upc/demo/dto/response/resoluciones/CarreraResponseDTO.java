67b4c0ae988d44c8ef1d32c649991259969140da
Tiag4 <longonjulian@gmail.com>
2026-09-16 21:55:14 -0300

    feat: construir DTOs de request y response para carrera

---
+++ src/main/java/com/upc/demo/dto/response/resoluciones/CarreraResponseDTO.java
@@ -0,0 +1,16 @@
+package com.upc.demo.dto.response.resoluciones;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarreraResponseDTO {
+
+    private Long idCarrera;
+    private String nombre;
+}
